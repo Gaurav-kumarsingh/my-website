fb291ad48f8cd10dd2b77a821bc27e139c754b96
Gaurav-kumarsingh <154734195+Gaurav-kumarsingh@users.noreply.github.com>
2026-04-04 23:31:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -519,7 +519,7 @@ function addBlankPage() {
 }
 
 window.onload = function () {
-  const url = "black_pages_landscapde.pdf"; // local file inside project
+  const url = "black_pages_landscape.pdf"; // local file inside project
 
   pdfjsLib.getDocument(url).promise.then((pdf) => {
     pdfDoc = pdf;
